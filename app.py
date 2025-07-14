4651528d4a7affeaf3883d17d699d22e9b208844
Jeevesh Rajan <itsjeeveshrajan@gmail.com>
2025-07-15 00:02:35 +0530

    Trigger CI/CD pipeline

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ QUOTES = [
 
 @app.route("/")
 def home():
-    return "Welcome to the Inspiring Quotes API!"
+    return "Welcome to the Inspiring Quotes API k8!"
 
 @app.route("/quote")
 def quote():
